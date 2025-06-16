1965b0ca18b538ffdf89113cda7ec225c78dd226
TanishqGupta22 <131939678+TanishqGupta22@users.noreply.github.com>
2025-06-16 11:00:33 +0530

    level2 completed

--- Array/Level2/src/LargestDigitsFinder.java
+++ Array/Level2/src/LargestDigitsFinder.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class LargestDigitsFinder {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner (System.in);
         int number;
         int maxDigit = 10;
         int[] digits = new int[maxDigit];

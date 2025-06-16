53c1786ebda514fbed1973ce6887c8f975c6c2fa
TanishqGupta22 <131939678+TanishqGupta22@users.noreply.github.com>
2025-06-16 10:58:59 +0530

    level2 completed

--- Array/Level2/src/DigitFrequency.java
+++ Array/Level2/src/DigitFrequency.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class DigitFrequency {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
